3804249cba881973eaf71b3e1d5929a8885248c4
Woo-yeong Kim <kimwy1997@gmail.com>
2023-03-14 21:04:10 +0900

    fix: resolve build errors

--- src/libs/vectorSimilarity.ts
+++
@@ -1,8 +0,0 @@
-export function vectorSimilarity(x: number[], y: number[]): number {
-  let sum = 0;
-  for (let i = 0; i < x.length; i++) {
-    // @ts-expect-error
-    sum += x[i] * y[i];
-  }
-  return sum;
-}
